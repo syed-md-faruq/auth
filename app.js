e7bedcd636a68a021bad0f9c470ae5167406527d
syed-md-faruq <faruq@divum.in>
2023-11-21 14:43:41 +0530

    new route2 added

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.urlencoded({extended: true}));
 app.get('/new',async(req,res) => {
     res.send("new route");
 })
+app.get('/new2',async(req,res) => {
+    res.send("new route 2");
+})
 
 app.get('/', verifyaccesstoken, async (req, res, next) => {
     res.send("Hello user");
